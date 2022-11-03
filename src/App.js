9344b93b7dd4f39e72a3cda62d90a255cca5d527
monyonko <monyonko.miriam@gmail.com>
2022-11-04 00:32:40 +0300

    Added vercel link

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function App() {
         
       </main>
       <footer>
-      <p>Coded by Kemuma, open-sourced at Github and hosted on Netlify.</p>
+      <p>Coded by <a href="https://github.com/monyonko">Kemuma</a>, open-sourced at <a href="https://github.com/monyonko/react-dictionary">Github</a> and hosted on Netlify.</p>
       </footer>
     </div>
   );
